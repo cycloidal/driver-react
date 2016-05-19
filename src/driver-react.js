8d593db369d5ea4d036a459ed0d86b26665cdf8e
Jakob Hohlfeld <jhohlfeld@polyban.io>
2016-05-19 17:43:19 +0200

    Add logic to extend intents selection to props.
    Any props from the parent props list may be selected using the following syntax:
    
    ```javascript
    connect('props!propName:prop(propTragetName)')
    ```
    
    - prefix `props!` differenciates this item from intents (default)
    - query `propName` is the query that selects the prop rom the parent props
    - local handler `prop(propTargetName)` tells the decorator where to put the selected prop
    
    Refer to https://www.npmjs.com/package/json-query on how to use queries and local handler functions.

--- src/driver-react.js
+++ src/driver-react.js
@@ -1,19 +1,32 @@
-import React, { createElement, Component, Children, PropTypes } from 'react'
+import React, {
+    createElement,
+    Component,
+    Children,
+    PropTypes
+} from 'react'
 import ReactDOM from 'react-dom'
+import jsonQuery from 'json-query'
 
-function getDisplayName(WrappedComponent) {
-    return WrappedComponent.displayName || WrappedComponent.name || 'Component'
-}
-
+/**
+ * Root component providing context.
+ *
+ * This component keeps reference to callbacks, which in this context are
+ * called intents. Each callbacks corresponds to one observable streams.
+ * These observables are called intent-streams.
+ *
+ * @class RxConnect
+ */
 class RxConnect extends Component {
 
     static childContextTypes = {
-        events: PropTypes.func
+        intents: PropTypes.func,
+        props: PropTypes.object
     }
 
     getChildContext() {
         return {
-            events: this.props.events
+            intents: this.props._intents,
+            props: this.props._props
         }
     }
 
@@ -23,59 +36,131 @@ class RxConnect extends Component {
 
 }
 
+/**
+ * Create a driver that connects to React components.
+ *
+ * @param {string} domTarget Selector where the app should be rendered to.
+ * @return {function} The driver function.
+ */
 function createReactDriver(domTarget) {
-    return function(vtree$) {
 
-        const _events = {}
+    /**
+     * This callback makes the driver.
+     *
+     * @param {Rx.Observable} state$ The stream characterizing the app state.
+     * @return {object} Interface to interact with thie driver.
+     */
+    return function(state$) {
+
+        const _intents$ = {}
         const _callbacks = {}
 
-        const events = function(type) {
-            if (!_events[type]) {
-                const event$ = new Rx.Subject()
-                _events[type] = event$
-                _callbacks[type] = (e) => event$.onNext(e)
+        /**
+         * Get/create the intent denoted by name param.
+         *
+         * @param {string} name The name of the intent.
+         * @return Rx.Subject
+         */
+        const intent = function(name) {
+            if (!_intents$[name]) {
+                const intent$ = new Rx.Subject()
+                _intents$[name] = intent$
+                _callbacks[name] = (e) => intent$.onNext(e)
             }
-            return _events[type]
+            return _intents$[name]
         }
 
-        const getCallback = function(type) {
-            if (!type) {
+        /**
+         * Get the callback attached to the intent name.
+         *
+         * @param {string} name The name of the intent.
+         * @return {function} Returns the attached callback function.
+         */
+        const getCallback = function(name) {
+            if (!name) {
                 return _callbacks
             }
-            return _callbacks[type]
+            return _callbacks[name]
         }
 
-        vtree$.subscribe(element => {
+        // subscribe to state updates
+        state$.subscribe(element => {
             ReactDOM.render(
-                createElement(RxConnect, { events: getCallback }, element),
+                createElement(RxConnect, {
+                    _intents: getCallback,
+                    _props: element.props
+                }, element),
                 document.querySelector(domTarget)
             )
         })
 
-        return { events }
+        // return the driver's interface
+        return {
+            intent
+        }
     }
 
 }
 
-function connect(...intents) {
+/**
+ * Context connect decorator.
+ *
+ * This decorater is used to provide a React.Component with the
+ * needed elements from a parent's context.
+ *
+ * @param {array} intents List of intents to connect through the context.
+ * @return {function} Decorator function.
+ */
+function connect(...args) {
+
+    const intentsArgs = args.filter(val => !val.match(/^\w+\!/))
+    const propsArgs = args.filter(val => val.match(/^props\!/))
+        .map(val => val.replace(/^props\!/, ''))
 
     return (WrappedComponent) => {
 
+        // helper to select displayName of a React.Component
+        const _dn = (comp) => comp.displayName || comp.name || 'Component'
+
+        /**
+         * Component decorator that connects to context.
+         *
+         * @class RxConnected
+         */
         return class RxConnected extends Component {
 
-            static displayName = `RxConnected(${getDisplayName(WrappedComponent)})`
+            static displayName = `RxConnected(${_dn(WrappedComponent)})`
 
             static contextTypes = {
-                events: PropTypes.func
+                intents: PropTypes.func,
+                props: PropTypes.object
             }
 
             render() {
 
-                const events = {}
-                intents.forEach(i => events[i] = this.context.events(i))
+                // transfer intents
+                const intents = {}
+                intentsArgs.forEach(i => intents[i] = this.context.intents(i))
+
+                // transfer other props
+                const props = {}
+                const locals = {
+                    prop: (input, target) => {
+                        props[target] = input
+                    }
+                }
+                propsArgs.forEach(prop => {
+
+                    jsonQuery(prop, {
+                        data: this.context.props,
+                        locals
+                    })
+
+                })
 
                 const mergedProps = {
-                    events,
+                    intents,
+                    ...props,
                     ...this.props
                 }
 
@@ -83,6 +168,7 @@ function connect(...intents) {
             }
 
         }
+
     }
 }
 
